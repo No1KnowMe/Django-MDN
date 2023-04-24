478dec03322fb5d8f63be795056c18d394044232
No1KnowMe <darthrazif@gmail.com>
2023-04-24 23:37:10 +0500

    Add Sign Up page form

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
